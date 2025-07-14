cb31554f5e3831b08672fc63e0f200516106eaaa
righasp <righasneistat@gmail.com>
2025-07-14 23:48:56 +0530

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Competition Website</h1>
+            <img src="path/to/logo.png" alt="Logo" />
+        </header>
+    );
+}
---
+++ src/components/Navigation.js
@@ -0,0 +1,14 @@
+export default function Navigation() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="problem-statement.html">Problem Statement</a></li>
+                <li><a href="methodology.html">Methodology</a></li>
+                <li><a href="implementation.html">Implementation</a></li>
+                <li><a href="progress.html">Progress</a></li>
+                <li><a href="team.html">Team</a></li>
+            </ul>
+        </nav>
+    );
+}
